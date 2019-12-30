768bf9180894bcee91858e9c3d874553619ab195
Chase Moskal <chasemoskal@gmail.com>
2019-12-29 19:04:27 -0800

    add: vscode settings, and convert to tabs

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@
 const form = document.querySelector("form.pokemon")
 
 form.addEventListener("change", () => {
-  for (showcase of document.querySelectorAll(".showcase")) {
-    showcase.removeAttribute("data-active")
-  }
+	for (showcase of document.querySelectorAll(".showcase")) {
+		showcase.removeAttribute("data-active")
+	}
 
-  const selectedRadioButton = form.querySelector("input[name=starters]:checked")
+	const selectedRadioButton = form.querySelector("input[name=starters]:checked")
 
-  if (selectedRadioButton) {
-    const {value} = selectedRadioButton
-    const showcase = document.querySelector(`.showcase[data-character="${value}"]`)
-    showcase.setAttribute("data-active", "")
-  }
+	if (selectedRadioButton) {
+		const {value} = selectedRadioButton
+		const showcase = document.querySelector(`.showcase[data-character="${value}"]`)
+		showcase.setAttribute("data-active", "")
+	}
 })
