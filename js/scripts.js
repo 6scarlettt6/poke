bf81763752561f4a31b54fb990b0f1f433d86bc6
scarlett <nickodelacruz96@gmail.com>
2019-12-30 04:05:07 -0800

    tried to make another form class pokemon but loop wouldn't work idk why.

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,23 @@ form.addEventListener("change", () => {
 		showcase.setAttribute("data-active", "")
 	}
 })
+
+// const form = document.querySelector("form.pokemon2")
+
+
+// form.addEventListener("change", () => {
+// 	for (showcase of document.querySelectorAll(".showcase")) {
+// 		showcase.removeAttribute("data-active")
+	
+
+// 	}
+// 	const selectedRadioButton = form.querySelector("input[name=starters]:checked")
+
+// 	if (selectRadioButton) {
+// 		const {value} = selectedRadioButton
+// 		const showcase = document.querySelector(`.showcase[data-character="${value}"]`)
+// 		showcase.setAttribute("data-active", "")
+// 	}
+
+// })
+
